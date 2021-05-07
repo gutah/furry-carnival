27270c04dfb14a87a6c0774929fbdf5b42f43cb6
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2021-05-06 21:28:27 -0300

    Initiatin BeautifoulSoup programming. Corrected ursl bugs

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+    
 from . import views
 
-app_name = 'polls'
+app_name = 'my_app'
 
 urlpatterns = [
     path('', views.home, name='index'),
+    path('new_search', views.new_search, name='new_search'), 
+
 ]
